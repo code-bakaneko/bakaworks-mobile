d76e90404318c0e50c7874d5dd94109512192437
Code Black <code.bakaneko@gmail.com>
2026-07-10 14:59:06 -0400

    rename supabase to supabaseAdmin

--- app/lib/supabase/admin.ts
+++ app/lib/supabase/admin.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 import 'server-only'
 
-export const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SECRET!, {
+export const supabaseAdmin = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SECRET!, {
   auth: {
     persistSession: false,
     autoRefreshToken: false,
