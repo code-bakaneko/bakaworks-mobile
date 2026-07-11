e50633a982a90baec60433dc442389f0ebaf183b
Code Black <code.bakaneko@gmail.com>
2026-07-11 13:01:23 -0400

    refactor: reorganize layout structure and add instructions for flashcard interaction

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,29 @@ export default async function Home() {
   const { data: vocabWords } = await supabase.from("language_vocabulary").select();
   return (
     <div
-      className="flex flex-col items-center p-4">
-        <header
-          className="h-20">
-
-        </header>
-      <FlashCard
-        words={vocabWords? vocabWords : []}
-      />
+      className="mx-auto flex px-4 gap-10 mt-10">
+        <aside className="">
+          <p>Upcoming Updates:</p>
+          <ol>
+            <li></li>
+          </ol>
+        </aside>
+        <main className="flex-1 flex flex-col items-center justify-center">
+          <div className="p-2">
+            <p>Instructions:</p>
+            <ol className="list-decimal">
+              <li>Guess/remember the translation</li>
+              <li>Click the card to flip</li>
+              <li>Answer correct if you got the translation correct</li>
+            </ol>
+          </div>
+          <FlashCard
+            words={vocabWords? vocabWords : []}
+          />
+          <footer>
+            {/*Advertisement */}
+          </footer>
+        </main>
     </div>
   );
 }
