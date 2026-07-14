c504d9a73b741b6dc293699e1943b6e249222fe0
Code Black <code.bakaneko@gmail.com>
2026-07-14 11:34:24 -0400

    restructure and style of multiple choice component
    button change to green to dispaly correct answer, and timer to wait 1 second before loading next index

--- app/components/multiplechoice.tsx
+++ app/components/multiplechoice.tsx
@@ -6,6 +6,7 @@ type Vocab = Tables<"language_vocabulary">;
 export default function MultipleChoice({ words }: { words:Vocab[] }) {
     const [index, setIndex] = useState(0);
     const [score, setScore] = useState(0);
+    const [revealed, setRevealed] = useState(false);
     const [choices, setChoices] = useState<Vocab[]>([]);
 
     useEffect(() => {
@@ -21,25 +22,31 @@ export default function MultipleChoice({ words }: { words:Vocab[] }) {
     if (words.length === 0) return <div>No words to use multiple choice</div>;
 
     function answer (choice: Vocab) {
+        setRevealed(true);
         if(choice.id === words[index].id) {
             setScore((score) => score + 1)
         }
-        setIndex((index) => (index + 1) % words.length)
+        setTimeout(() => {
+            setIndex((index) => (index + 1) % words.length)
+            setRevealed(false);
+        }, 1000);
     }
     
     return (
         <div className="w-96 bg-slate-950 p-10 border border-slate-800 rounded-sm">
             <div className="flex flex-col gap-10">
-                <p>Score: {score}</p>
-                <div>
+                <p className="text-muted">Score: {score}</p>
+                <div className="flex flex-col items-center gap-2">
                     <p>{words[index].romanized}</p>
                     <p>{words[index].reading}</p>
-                    <p>{words[index].foreign_word}</p>
+                    <p className="text-2xl font-bold">{words[index].foreign_word}</p>
                 </div>
-                <div className="flex gap-5">
+                <div className="grid grid-cols-2 gap-3">
                     {choices.map((choice) => (
                         <button key={choice.id} onClick={() => answer(choice)}
-                            className="bg-brand px-4 py-2 h-10 rounded-sm border-b-4 border-brand-dark">
+                            className={`
+                                ${revealed && choice.id === words[index].id ? "bg-green-500 border-green-700": "bg-brand border-brand-dark"} px-4 py-2 min-h-10 rounded-sm border-b-4
+                                hover:cursor-pointer hover:translate-y-1 transition-all hover:border-0`}>
                             {choice.english_word}
                         </button>
                     ))}
