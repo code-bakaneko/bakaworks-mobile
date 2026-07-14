f4a7017d9d84db5998ae1336a2bb02f0e019bbca
Code Black <code.bakaneko@gmail.com>
2026-07-13 20:25:40 -0400

    finished multiple choice question

--- app/components/multiplechoice.tsx
+++ app/components/multiplechoice.tsx
@@ -15,16 +15,32 @@ export default function MultipleChoice({ words }: { words:Vocab[] }) {
             .slice(0, 3);
             return [correct, ...others].sort(() => Math.random() - 0.5);
     }, [index, words]);
+
+    function answer (choice: Vocab) {
+        if(choice.id === words[index].id) {
+            setScore((score) => score + 1)
+        }
+        setIndex((index) => (index + 1) % words.length)
+    }
     
     return (
-        <div>
-            <p>Score: {score}</p>
-            <p>{words[index].foreign_word}</p>
-            {choices.map((choice) => (
-                <button key={choice.id}>
-                    {choice.english_word}
-                </button>
-            ))}
+        <div className="bg-slate-950 p-10 border rounded-sm">
+            <div className="flex flex-col gap-10">
+                <p>Score: {score}</p>
+                <div>
+                    <p>{words[index].romanized}</p>
+                    <p>{words[index].reading}</p>
+                    <p>{words[index].foreign_word}</p>
+                </div>
+                <div className="flex gap-5">
+                    {choices.map((choice) => (
+                        <button key={choice.id} onClick={() => answer(choice)}
+                            className="bg-brand px-4 py-2 h-10 rounded-sm border-b-4 border-brand-dark">
+                            {choice.english_word}
+                        </button>
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
